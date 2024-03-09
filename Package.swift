24569dec176c1e5638009c36e225d647df6dcf06
smease12 <scmease93@gmail.com>
2024-03-09 18:05:09 -0500

    Update package.swift to add dependencies section

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "FlashChat",
             targets: ["FlashChat"]),
     ],
+    dependencies: [
+          // Dependencies declare other packages that this package depends on.
+      ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
