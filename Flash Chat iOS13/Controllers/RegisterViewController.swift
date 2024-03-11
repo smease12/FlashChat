2b311ee4f4c5d26b167748f72d8b92636eb124e6
smease12 <scmease93@gmail.com>
2024-03-10 21:52:10 -0400

    Print error if user authentication doesn't work

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -18,6 +18,9 @@ class RegisterViewController: UIViewController {
         
         if let email = emailTextfield.text, let password = passwordTextfield.text{
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                }
             }
         }
     }
