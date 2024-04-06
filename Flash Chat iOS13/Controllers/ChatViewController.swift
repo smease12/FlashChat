2f4ec2b3d3ff533b82e0cbef840f6203fa58605f
smease12 <scmease93@gmail.com>
2024-04-05 21:13:59 -0400

    Create UITableViewDataSource extension in ChatViewController

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -23,6 +23,8 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
     }
@@ -38,5 +40,15 @@ class ChatViewController: UIViewController {
           print("Error signing out: %@", signOutError)
         }
     }
+}
+
+extension ChatViewController: UITableViewDataSource{
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+    }
 }
