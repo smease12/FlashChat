fef286c78378235424d0a779379025bfea661a4f
smease12 <scmease93@gmail.com>
2024-04-19 20:49:29 -0400

    Remove auto toolbar from IQKeyboardManager

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print(db)
         
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
         
         return true
     }
