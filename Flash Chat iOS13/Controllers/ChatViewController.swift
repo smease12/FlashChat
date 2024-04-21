e5d17f45686a8a423fbe38d3bc2b57013875761a
smease12 <scmease93@gmail.com>
2024-04-20 22:13:26 -0400

    Update message appearance when sender is not current user

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -106,6 +106,13 @@ extension ChatViewController: UITableViewDataSource{
             cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
             cell.label.textColor = UIColor(named: K.BrandColors.purple)
         }
+        //This is a message from another sender
+        else{
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
         
         return cell
     }
